384bd16aa50a39d287f1f0e1327e440fac227b9b
cool-hammer <ajtwlstmdgks@gmail.com>
2021-04-19 14:57:50 +0900

    embedì, ëevent id를 footer에 추가

--- bot.py
+++ bot.py
@@ -30,7 +30,8 @@ async def get_events(ctx, maxResult=5):
 
         embed = discord.Embed(title=event.get('summary'),
                               url=event.get('htmlLink'),
-                              description=f'{start} ~ {end}\nid: {event.get("id")}')
+                              description=f'{start} ~ {end}')
+        embed.set_footer(text=f'id: {event.get("id")}')
         await ctx.send(embed=embed)
 
     await ctx.send('가져오기 완료!')
@@ -63,8 +64,40 @@ async def create_event(ctx, summary, startdate, enddate, freq="DAILY", count=1):
     embed = discord.Embed(title=event.get('summary'),
                           url=event.get('htmlLink'),
                           description=f'{start} ~ {end}\nid: {event.get("id")}')
+    embed.set_footer(text=f'id: {event.get("id")}')
     await ctx.send('일정을 생성하였습니다.', embed=embed)
     # await ctx.send(f'일정이 생성되었습니다: {event.get("htmlLink")}')
+
+
+@app.command(aliases=['일정수정'])
+async def update_event(ctx, eventId, key, value):
+    await ctx.send('일정 수정 중입니다...')
+
+    event = calendar.events().get(calendarId='primary', eventId=eventId).execute()
+
+    if key == '제목':
+        event['summary'] = value
+    elif key == '시작일':
+        event['start']['date'] = value
+    elif key == '종료일':
+        event['end']['date'] = value
+
+    updated_event = calendar.events().update(calendarId='primary', eventId=eventId, body=event).execute()
+
+    start = event['start'].get('dateTime', event['start'].get('date'))
+    end = event['end'].get('dateTime', event['end'].get('date'))
+    embed = discord.Embed(title=event.get('summary'),
+                          url=event.get('htmlLink'),
+                          description=f'{start} ~ {end}\nid: {event.get("id")}')
+    embed.set_footer(text=f'id: {event.get("id")}')
+    await ctx.send('일정이 수정되었습니다.', embed=embed)
+
+
+@app.command(aliases=['일정삭제'])
+async def delete_event(ctx, eventId):
+    await ctx.send('일정 삭제 중입니다...')
+    calendar.events().delete(calendarId='primary', eventId=eventId).execute()
+    await ctx.send('일정이 삭제되었습니다.')
 
 
 @app.event
